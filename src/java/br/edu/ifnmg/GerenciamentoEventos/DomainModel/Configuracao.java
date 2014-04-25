e0d1a85720e7470cc77863b26bb0e5722ca72308
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2014-04-25 15:35:41 -0300

    Correção do bug: Tornar Padrão do Usuário não está funcionando

--- src/java/br/edu/ifnmg/GerenciamentoEventos/DomainModel/Configuracao.java
+++ src/java/br/edu/ifnmg/GerenciamentoEventos/DomainModel/Configuracao.java
@@ -9,20 +9,20 @@ package br.edu.ifnmg.GerenciamentoEventos.DomainModel;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Cacheable;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Index;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
+
 
 /**
  *
@@ -30,7 +30,7 @@ import javax.persistence.Version;
  */
 @Cacheable
 @Entity
-@Table(name="configuracoes", indexes = {@Index(columnList = "chave", unique = true)})
+@Table(name="configuracoes", uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario","chave"})})
 public class Configuracao implements Entidade, Serializable {
     private static final long serialVersionUID = 1L;
     @Id
