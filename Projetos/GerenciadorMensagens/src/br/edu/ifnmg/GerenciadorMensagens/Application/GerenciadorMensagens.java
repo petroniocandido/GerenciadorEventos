95bbddf32ea07e8b8c645d4abc0cbbc45e8f0f2d
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2014-09-26 09:38:32 +0000

    Issue #73 - Implementação final

--- Projetos/GerenciadorMensagens/src/br/edu/ifnmg/GerenciadorMensagens/Application/GerenciadorMensagens.java
+++ Projetos/GerenciadorMensagens/src/br/edu/ifnmg/GerenciadorMensagens/Application/GerenciadorMensagens.java
@@ -17,46 +17,49 @@ import java.util.List;
  */
 public class GerenciadorMensagens {
 
-    public static void main(String[] args) {
-        MensagemRepositorio mDAO = new MensagemDAO();
-        MailService mail = new MailServiceImpl();
-        br.edu.ifnmg.DomainModel.Services.LogService log = new LogServiceImpl();
-        int total=0, totalEnviado=0, totalNaoEnviado=0, totalErro=0;
-        try {
-            List<Mensagem> mensagens = mDAO.Buscar();
-            total = mensagens.size();
-            for (Mensagem m : mensagens) {
-                try {
-                    if (mail.enviar(m.getDestinatario(), m.getAssunto(), m.getCorpo())) {
-                        totalEnviado++;
-                        mDAO.Apagar(m);
-                    } else {
-                        totalNaoEnviado++;
-                        if (m.getNumeroTentativas() <= 5) {
-                            m.setNumeroTentativas(m.getNumeroTentativas() + 1);
-                        } else {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            Thread.sleep(1000 * 60 * 3);
+            try {
+                MensagemRepositorio mDAO = new MensagemDAO();
+                MailService mail = new MailServiceImpl();
+                br.edu.ifnmg.DomainModel.Services.LogService log = new LogServiceImpl();
+                int total = 0, totalEnviado = 0, totalNaoEnviado = 0, totalErro = 0;
+
+                List<Mensagem> mensagens = mDAO.Buscar();
+                total = mensagens.size();
+                for (Mensagem m : mensagens) {
+                    try {
+                        if (mail.enviar(m.getDestinatario(), m.getAssunto(), m.getCorpo())) {
+                            totalEnviado++;
                             mDAO.Apagar(m);
-                            log.Append("A mensagem \'" + m.getAssunto() + "\" para o destinatário \"" + m.getDestinatario()
-                                    + " está sendo removida após 5 tentativas de envio sem êxito!");
+                        } else {
+                            totalNaoEnviado++;
+                            if (m.getNumeroTentativas() <= 5) {
+                                m.setNumeroTentativas(m.getNumeroTentativas() + 1);
+                            } else {
+                                mDAO.Apagar(m);
+                                log.Append("A mensagem \'" + m.getAssunto() + "\" para o destinatário \"" + m.getDestinatario()
+                                        + " está sendo removida após 5 tentativas de envio sem êxito!");
+                            }
                         }
+                    } catch (Exception ex) {
+                        totalErro++;
+                        log.Append("Erro ao processar a mensagem : " + ex.getMessage());
+                        System.out.println("Erro ao processar a mensagem : " + ex.getMessage());
                     }
-                } catch (Exception ex) {
-                    totalErro++;
-                    log.Append("Erro ao processar a mensagem : " + ex.getMessage());
-                    System.out.println("Erro ao processar a mensagem : " + ex.getMessage());
                 }
+
+                log.Append("Término da execução do Gerenciador de Mensagens: " + total + " mensagens, "
+                        + totalEnviado + " enviadas, " + totalNaoEnviado + " não enviadas, "
+                        + totalErro + " erros.");
+                System.out.println("Término da execução do Gerenciador de Mensagens: " + total + " mensagens, "
+                        + totalEnviado + " enviadas, " + totalNaoEnviado + " não enviadas, "
+                        + totalErro + " erros.");
+            } catch (Exception ex) {
+                System.out.println("Erro no Gerenciador de Mensagens: " + ex.getMessage());
             }
-        } catch (Exception ex) {
-            totalErro++;
-            log.Append("Erro no Gerenciador de Mensagens: " + ex.getMessage());
-            System.out.println("Erro no Gerenciador de Mensagens: " + ex.getMessage());
         }
-        log.Append("Término da execução do Gerenciador de Mensagens: " + total + " mensagens, " 
-                + totalEnviado + " enviadas, "+ totalNaoEnviado + " não enviadas, "
-                + totalErro + " erros.");
-        System.out.println("Término da execução do Gerenciador de Mensagens: " + total + " mensagens, " 
-                + totalEnviado + " enviadas, "+ totalNaoEnviado + " não enviadas, "
-                + totalErro + " erros.");
     }
 
 }
